4b8768dffdc862e3ea5776ddd8c2b151a20defd4
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2021-02-20 13:20:36 +0700

    Add files via upload

---
+++ UAS-10117125-ZULFIIHZAMRAHMAT-6/src/uas/pkg10117125/zulfiihzamrahmat/pkg6/Kalkulator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.pkg10117125.zulfiihzamrahmat.pkg6;
+
+/**
+ * Nama  : Zulfi Ihzam Rahmat
+ * NIM   : 10117125
+ * Kelas : IF-1
+ * 
+ */
+public class Kalkulator {
+    private double value1, value2;
+
+    public double getValue1() {
+        return value1;
+    }
+
+    public void setValue1(double value1) {
+        this.value1 = value1;
+    }
+
+    public double getValue2() {
+        return value2;
+    }
+
+    public void setValue2(double value2) {
+        this.value2 = value2;
+    }
+
+    public double multiplication(double value1, double value2) {
+        return value1 * value2;
+    }
+    
+}
